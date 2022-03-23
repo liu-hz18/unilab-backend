4c578def78f50098215b20f20e5a6faf0ff9ba77
liuhz <1456437967@qq.com>
2022-03-24 00:11:12 +0800

    add jwt support to maintain user login state

---
+++ apis/login.go
@@ -0,0 +1,52 @@
+package apis
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
+
+	"unilab-backend/database"
+	"unilab-backend/jwt"
+)
+
+// login router
+type login struct {
+	userid string `validate:"required,len=10"`
+	password string `validate:"required"`
+}
+
+// user login
+func UserLoginHandler(c *gin.Context) {
+	userid := c.PostForm("userid")
+	password := c.PostForm("password")
+	log.Printf("user id: %s, password: %s", userid, password)
+	validator := validator.New()
+	loginInfo := login{userid: userid, password:password}
+	err := validator.Struct(&loginInfo)
+	code := INVALID_PARAMS
+	data := make(map[string]interface{})
+	if err != nil { // code = INVALID_PARAMS
+		log.Println(err)
+	} else {
+		// 用于gitlab验证
+		existed := database.CheckUserExist(userid)
+		if existed {
+			token, err := jwt.TokenGenerator(userid, password)
+			if err != nil {
+				code = ERROR_AUTH_TOKEN
+			} else {
+				data["token"] = token
+				code = SUCCESS
+			}
+		} else {
+			code = ERROR_AUTH
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg": MsgFlags[code],
+		"data": data,
+	})
+}
